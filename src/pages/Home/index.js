ab45d9d8c58bc20a7940e202855a0ca9ef59c60f
wagner <wagnercl31@hotmail.com>
2020-07-30 17:37:25 -0300

    Controle de rotas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu'
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import dadosIniciais from './data/dados_iniciais.json';
+import Menu from '../../components/Menu'
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Footer from '../../components/Footer';
 
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414"}}>
       <Menu />
@@ -40,12 +41,10 @@ function App() {
       <Carousel 
         category={dadosIniciais.categorias[5]}
       />
-      
-      
 
-
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
